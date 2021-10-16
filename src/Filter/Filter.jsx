ac64320c874019d1410666832389344d60ebb6ab
Maksim Chekhovskiy <veles.cafe@gmail.com>
2021-10-16 20:14:16 +0300

    add mount and update methods

---
+++ src/Filter/Filter.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import "./Filter.css";
+
+class Filter extends Component {
+  setFilterValue = event => {
+    let value = event.currentTarget.value.toUpperCase();
+    this.props.setFilterToState(value);
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>Find contacts by name :</h4>
+        <input   onChange={this.setFilterValue}></input>
+        
+      </div>
+    );
+  }
+}
+
+Filter.propTypes = {
+  setFilterToState: PropTypes.func.isRequired,
+};
+export default Filter;
